30de8815558f7f0e2999e9310f781a9c236ff14d
irfanhaider5590 <68530688+irfanhaider5590@users.noreply.github.com>
2022-09-30 15:08:13 +0500

    movie rating app using microservices

---
+++ rating-data-service/src/main/java/io/javabrains/ratingdataservice/resources/RatingsResource.java
@@ -0,0 +1,40 @@
+package io.javabrains.ratingdataservice.resources;
+
+import io.javabrains.ratingdataservice.models.Rating;
+import io.javabrains.ratingdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+//    @RequestMapping("/user/{userId}")
+//    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+//        UserRating userRating = new UserRating();
+//        userRating.initData(userId);
+//        return userRating;
+//
+//    }
+    @RequestMapping("/user/{userId}")
+    public UserRating getRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings= Arrays.asList(
+                new Rating("1234",4),
+                new Rating("3212",3)
+        );
+        UserRating userRating= new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+
+    }
+
+}
