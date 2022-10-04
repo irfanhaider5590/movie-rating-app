e2c078bd8dbd0ca7995ea3d7f7bd7a45811ec692
Irfan Haider <irfanhaider5590@gmail.com>
2022-10-04 11:15:12 +0500

    Added external movieDb API to call movie info

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/models/Movie.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/models/Movie.java
@@ -1,12 +1,14 @@
 package io.javabrains.movieinfoservice.models;
 
 public class Movie {
-    String movieId;
-    String movieName;
+    private String movieId;
+    private String name;
+    private String description;
 
-    public Movie(String movieId, String movieName) {
+    public Movie(String movieId, String name, String description) {
         this.movieId = movieId;
-        this.movieName = movieName;
+        this.name = name;
+        this.description = description;
     }
 
     public String getMovieId() {
@@ -17,11 +19,19 @@ public class Movie {
         this.movieId = movieId;
     }
 
-    public String getMovieName() {
-        return movieName;
+    public String getName() {
+        return name;
     }
 
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
